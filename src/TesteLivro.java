bc15c85c106feb607f98f299ec1645f5cd9c6e3b
Hectorsalgadoo <hectorsoria2@outlook.com>
2025-03-13 12:22:08 -0300

    Projeto livraria

---
+++ src/TesteLivro.java
@@ -0,0 +1,19 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+        //criar um objeto-> instanciar um objeti
+        // sintaxe: tipo nomeObjeto=new Tipo();
+                    Livro meuLivro= new Livro();
+                    meuLivro.titulo = "UML 2 ";
+                    meuLivro.autor="Gilleanes Guedes";
+                    meuLivro.editora="novatec";
+                    meuLivro.preco=50;
+                    meuLivro.paginas=484;
+                    meuLivro.tipoCapa="Capa dura";
+                    meuLivro.resumo="UML abordagem pratica...";
+
+
+           Livro livroDoHector = new Livro();
+           livroDoHector.titulo="Herry potter e a pedra filosofal";
+           
+    }
+}
